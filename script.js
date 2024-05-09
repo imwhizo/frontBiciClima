ebd59a3fd4f4ce539f917e57b7751bf45914d0dd
imwhizo <gallegosalbertodiego30@gmail.com>
2024-05-08 22:44:17 -0600

    Avance del Front

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// Ejemplo de solicitud a una API de clima utilizando Fetch API
-fetch('URL_DE_LA_API_DEL_CLIMA')
+fetch('http://localhost:8000/')
     .then(response => response.json())
-    .then(data => {
-        // Procesar los datos y actualizar la interfaz de usuario
-    })
-    .catch(error => console.error('Error al obtener los datos del clima:', error));
+    .then(data => console.log(data));
